131b129c6846cb5cfb7d743bde00f8e751680e55
Jim Marshall <jim.marshall@codethink.co.uk>
2025-12-21 23:54:18 +0000

    dox file script added

---
+++ Stardew/engine/scripts/CreateDoxFiles.py
@@ -0,0 +1,30 @@
+import os
+
+# wrap the markdown docs in this .dox bullshit
+# a custom script shouldn't be necesary for this but i can't get the \include feature of doxygen to work
+
+template = """
+
+/** \page <<PAGE_NAME>>
+ 
+<<PAGE_CONTENT>>
+ 
+*/
+
+"""
+
+def main():
+    for file in os.listdir("../docs"):
+        path = os.path.join("../docs", file)
+        outPath = os.path.join("../src", file).replace(".md", ".dox")
+        print(path)
+        r = ""
+        with open(path, "r") as f:
+            md = f.read()
+            r = template.replace("<<PAGE_NAME>>", file[:-3])
+            r = r.replace("<<PAGE_CONTENT>>", md)
+        with open(outPath, "w") as f:
+            f.write(r)
+    pass
+
+main()
