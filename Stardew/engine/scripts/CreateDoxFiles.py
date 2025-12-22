4d4592ea169fa12b9a7e11f9924278f4024d4421
Jim Marshall <jim.marshall@codethink.co.uk>
2025-12-22 00:41:25 +0000

    fixup

--- Stardew/engine/scripts/CreateDoxFiles.py
+++ Stardew/engine/scripts/CreateDoxFiles.py
@@ -17,15 +17,14 @@ def main():
     data = ""
     for file in os.listdir("../docs"):
         path = os.path.join("../docs", file)
-        outPath = os.path.join("../src", file).replace(".md", ".dox")
         print(path)
         r = ""
         with open(path, "r") as f:
             md = f.read()
-            r = template.replace("<<PAGE_NAME>>", file[:-3])
-            r = r.replace("<<PAGE_CONTENT>>", md)
-            data += r
+            data += md
             data += "\n"
+
+    data = template.replace("<<PAGE_CONTENT>>", data)
 
     with open("../src/mainpage.dox", "w") as f:
         f.write(data)
