52a26910affa0dc82d280cd47965bbaaef6ca900
Jim Marshall <jim.marshall@codethink.co.uk>
2026-01-01 23:50:09 +0000

    comments

--- Stardew/engine/include/NetworkID.h
+++ Stardew/engine/include/NetworkID.h
@@ -1,7 +1,12 @@
 #ifndef NETWORKID_H
 #define NETWORKID_H
 
+/// @brief Call this when you deserialize a new net id sent from the server - games won't ever have to call it
+/// @param nextVal 
 void NetID_DeserializedNewID(int nextVal);
+
+/// @brief Get a new net ID - the authoritative ID an entity has, assigned by the server but guessed by the client
+/// @return the network ID, assign to Entity2D::networkID
 int NetID_GetID();
 
 #endif
