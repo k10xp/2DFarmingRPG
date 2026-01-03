715a799ced34dc99e3e9010fa971feff909863db
Jim Marshall <jim.marshall@codethink.co.uk>
2026-01-03 16:03:34 +0000

    try to make windows build produce a full playable packaged game

--- Stardew/compile_assets.bat
+++ Stardew/compile_assets.bat
@@ -1,7 +1,14 @@
-python engine/scripts/ConvertTiled.py ./Assets/out_rle --rle true -m ./Assets/Farm.json ./Assets/House.json ./Assets/RoadToTown.json -a engine/scripts/AtlasTool.exe
 
 python engine/scripts/ConvertTiled.py ./Assets/out -m ./Assets/Farm.json ./Assets/House.json ./Assets/RoadToTown.json -a engine/scripts/AtlasTool.exe -bmp Atlas.bmp -iw 128 -ih 128
 
+python ./engine/scripts/ExpandAnimations.py -o ./Assets/out/expanded_named_sprites.xml ./Assets/out/named_sprites.xml
 
-"engine/scripts/AtlasTool.exe" ./Assets/ui_atlas.xml -o ./Assets/ui_atlas.atlas -bmp UIAtlas.bmp -iw 128 -ih 128
+python engine/scripts/MergeAtlases.py ./Assets/out/atlas.xml ./Assets/out/expanded_named_sprites.xml > ./Assets/out/atlascombined.xml
+
+"engine/scripts/AtlasTool.exe" ./Assets/out/atlascombined.xml -o ./Assets/out/main.atlas
+
+"engine/scripts/AtlasTool.exe" ./Assets/ui_atlas.xml -o ./Assets/ui_atlas.atlas
+
+robocopy "./Assets" "game\Release\Assets" /E /XO
+
 pause
